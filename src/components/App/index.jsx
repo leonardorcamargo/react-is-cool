7a1590dd0262f1178956ea175f2fd3645a019153
leonardorcamargo <leonardorcamargo@gmail.com>
2019-01-14 13:02:54 -0200

    Instalado modulo react-bootstrap para criar layout da tela onde será exibido dados de presença na sala de reuniões.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,63 @@
 import React from 'react';
-import Clock from '../Clock'
+import { Table, Panel, FormGroup, FormControl, Form, Col, Checkbox } from 'react-bootstrap';
+import axios from 'axios';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Clock 
-                    name="Leonardo"
-                    phrase="LIER!"
-                />
+                <Panel bsStyle="default">
+                    <Panel.Heading>
+                        <Panel.Title componentClass="h2">Controle de acesso a Sala de Reuniões</Panel.Title>
+                    </Panel.Heading>
+                    <Panel.Body>
+                        <Form horizontal>
+                            <FormGroup controlId="formControlFilter">
+                                <Col sm={4} md={3}>
+                                    <FormControl componentClass="select" placeholder="select">
+                                        <option value="all">Exibir tudo</option>
+                                        <option value="busy">Somente ocupados</option>
+                                        <option value="free">Somente disponíveis</option>
+                                    </FormControl>
+                                </Col>
+                                <Col sm={3}>
+                                    <Checkbox>Agrupar por Situação</Checkbox>
+                                </Col>
+                            </FormGroup>
+                        </Form>                        
+                        <Table striped bordered hover responsive>
+                            <thead>
+                                <tr>
+                                    <th>Inicio da análise</th>
+                                    <th>Fim da análise</th>
+                                    <th>Situação</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{Date.now()}</td>
+                                    <td>{Date.now()}</td>
+                                    <td>Liberado</td>
+                                </tr>
+                                <tr>
+                                    <td>{Date.now()}</td>
+                                    <td>{Date.now()}</td>
+                                    <td>Liberado</td>
+                                </tr>
+                                <tr>
+                                    <td>{Date.now()}</td>
+                                    <td>{Date.now()}</td>
+                                    <td>Liberado</td>
+                                </tr>
+                                <tr>
+                                    <td>{Date.now()}</td>
+                                    <td>{Date.now()}</td>
+                                    <td>Liberado</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Panel.Body>
+                </Panel>
             </div>
         )
     }
