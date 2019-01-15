e9192776124dcbe94d83c0a762cfd3e582c91085
leonardorcamargo <leonardorcamargo@gmail.com>
2019-01-15 19:45:44 -0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b67622f35017e0f6b58f93b4e4cd4407.js"
+  "/react-is-cool/precache-manifest.769c76d24af6ce880563c479dd1f3abf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-is-cool/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
--- src/utils/uuid.js
+++
@@ -1,7 +0,0 @@
-const crypto = require('crypto');
-
-const uuid = () => {
-    return crypto.randomBytes(16).toString("hex");
-}
-
-module.exports = uuid;
